78758675c9ccf34c9df214ff3221365f242e5dc7
kp2c <kfirp84@gmail.com>
2022-07-19 21:32:10 +0300

    attempt to fix github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from "@sveltejs/adapter-static"; 
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -16,7 +18,10 @@ const config = {
 			pages: 'docs',
 			assets: 'docs',
 			fallback: true
-		})
+		}),
+		paths: {
+			base: dev ? '' : '/portfolio',
+		}
 	}
 };
 
