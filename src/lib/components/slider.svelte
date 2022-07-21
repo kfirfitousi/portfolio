a6239b38557c36ada31c0b6fa1adc88159ffdd5d
kp2c <kfirp84@gmail.com>
2022-07-21 17:57:46 +0300

    Fixed slider story not showing car

--- src/lib/components/slider.svelte
+++ src/lib/components/slider.svelte
@@ -3,7 +3,7 @@
     
     export let car = "/car.webp";
     export let offsetX = 0;
-    export let trackWidth;
+    export let trackWidth = 0;
     let windowWidth: number;
 
     $: position = {
@@ -20,7 +20,8 @@
 <svelte:window bind:innerWidth={windowWidth} />
 
 <div bind:clientWidth={trackWidth} class="flex items-end w-full h-2 md:h-3 bg-gray-600 mx-auto rounded-t-lg select-none">
-    <div class="w-16 h-6 mb-0.5 md:w-20 md:h-8 md:mb-0 bg-[url({ car })] bg-cover bg-no-repeat"
+    <div class="w-16 h-6 mb-0.5 md:w-20 md:h-8 md:mb-0 bg-cover bg-no-repeat"
+        style="background-image: url({ car })"
         use:draggable={{
             axis: 'x',
             bounds: 'parent',
