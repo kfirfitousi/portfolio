2a792477685b395cb9ecb7a1d7a2229417334409
kp2c <kfirp84@gmail.com>
2022-07-23 15:30:32 +0300

    Set an explicit width and height on footer images

--- src/lib/components/Footer/Footer.svelte
+++ src/lib/components/Footer/Footer.svelte
@@ -2,10 +2,10 @@
 	<div class="flex flex-col text-center text-zinc-600 dark:text-zinc-300">
 		<div class="flex flex-row flex-wrap md:flex-nowrap justify-center items-center mx-auto my-2">
 			<span class="mb-2 md:mr-2 md:mb-0 w-full md:w-auto">Built with</span>
-			<img src="https://img.shields.io/badge/svelte-%23f1413d.svg?style=for-the-badge&logo=svelte&logoColor=white" alt="Svelte">
-			<img src="https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white" alt="Typescript">
-			<img src="https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white" alt="TailwindCSS">
-			<img src="https://img.shields.io/badge/vercel-%23000000.svg?style=for-the-badge&logo=vercel&logoColor=white" alt="Vercel">
+			<img src="https://img.shields.io/badge/svelte-%23f1413d.svg?style=for-the-badge&logo=svelte&logoColor=white" alt="Svelte" width="92.5" height="28">
+			<img src="https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white" alt="Typescript" width="126.5" height="28">
+			<img src="https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white" alt="TailwindCSS" width="138.75" height="28">
+			<img src="https://img.shields.io/badge/vercel-%23000000.svg?style=for-the-badge&logo=vercel&logoColor=white" alt="Vercel" width="93.5" height="28">
 		</div>
 		<span>
 			Source code available on
