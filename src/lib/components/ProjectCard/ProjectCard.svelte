8014cffcffcc22fd52926fdfc6f438ed22868684
kp2c <kfirp84@gmail.com>
2022-07-23 14:56:08 +0300

    Changed some light mode colors

--- src/lib/components/ProjectCard/ProjectCard.svelte
+++ src/lib/components/ProjectCard/ProjectCard.svelte
@@ -12,7 +12,7 @@
     <h1 class="text-xl h-10 leading-10 text-center bg-rose-700 dark:bg-pink-900 text-zinc-100 rounded-t-lg">{title}</h1>
     <div class="flex flex-col sm:flex-row">
         <img src={image} alt={title} class="h-full aspect-square sm:h-[22rem] rounded-bl-none sm:rounded-bl-lg" loading={lazyImage ? "lazy" : null} />
-        <div class="flex flex-col justify-center w-full p-2 sm:w-1/2 bg-rose-50 dark:bg-zinc-700 text-rose-700 dark:text-zinc-100 rounded-b-lg sm:rounded-bl-none">
+        <div class="flex flex-col justify-center w-full p-2 sm:w-1/2 bg-zinc-50 dark:bg-zinc-700 text-rose-700 dark:text-zinc-100 rounded-b-lg sm:rounded-bl-none">
             <p class="text-xl text-center">{subtitle}</p>
             <p class="text-center mb-2">{summary}</p>
             <p class="flex flex-row mx-auto mb-2 sm:mb-0 space-x-2"> 
