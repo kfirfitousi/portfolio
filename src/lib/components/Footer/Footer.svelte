c7686af68b16ea120fcfd59aa3fd7881e31afab4
kfirfitousi <kfirp84@gmail.com>
2022-08-03 17:45:16 +0300

    Updated footer

--- src/lib/components/Footer/Footer.svelte
+++ src/lib/components/Footer/Footer.svelte
@@ -6,7 +6,7 @@
             <span class="mb-2 md:mr-2 md:mb-0 w-full md:w-auto">Built with</span>
             <img
                 src="https://img.shields.io/badge/svelte-%23f1413d.svg?style=for-the-badge&logo=svelte&logoColor=white"
-                class="bw:grayscale"
+                class="bw:grayscale px-[0.4rem] w-fit bg-[#f1413d]"
                 alt="Svelte"
                 width="92.5"
                 height="28"
@@ -37,14 +37,14 @@
             Source code available on
             <a
                 href="https://github.com/kfirfitousi/portfolio"
-                class="underline text-accent hover:text-primary dark:text-accent-contrast dark:hover:text-rose-600"
+                class="underline text-accent hover:text-primary dark:text-accent-contrast sky:hover:text-rose-700 dark:hover:text-rose-600"
                 target="_blank">GitHub</a
             >.
         </span>
         <span>
             Icons by <a
                 href="https://icons8.com"
-                class="underline text-accent hover:text-primary dark:text-accent-contrast dark:hover:text-rose-600"
+                class="underline text-accent hover:text-primary dark:text-accent-contrast sky:hover:text-rose-700 dark:hover:text-rose-600"
                 target="_blank">Icons8</a
             >.
         </span>
